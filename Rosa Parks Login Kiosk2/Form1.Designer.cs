3e10d794e26094904bb4b1b1d70e013c608c4fbd
Jeremiah O'Neal <joneal97@hotmail.com>
2025-02-20 08:02:36 -0800

    Adding new files.

--- Rosa Parks Login Kiosk2/Form1.Designer.cs
+++ Rosa Parks Login Kiosk2/Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
             this.currentTime = new System.Windows.Forms.Label();
             this.aboutButton = new System.Windows.Forms.Button();
             this.loggedinCount = new System.Windows.Forms.Label();
@@ -40,7 +41,6 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button2 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.button4 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -49,9 +49,10 @@
             // 
             this.button1.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(725, 110);
+            this.button1.Location = new System.Drawing.Point(1088, 169);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(134, 83);
+            this.button1.Size = new System.Drawing.Size(201, 128);
             this.button1.TabIndex = 0;
             this.button1.Text = "Login";
             this.button1.UseVisualStyleBackColor = true;
@@ -62,9 +63,10 @@
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(72, 16);
+            this.label1.Location = new System.Drawing.Point(108, 25);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(734, 73);
+            this.label1.Size = new System.Drawing.Size(1080, 108);
             this.label1.TabIndex = 1;
             this.label1.Text = "Rosa Parks Login Kiosk";
             this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -81,30 +83,45 @@
             this.groupBox1.Controls.Add(this.button2);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.button1);
-            this.groupBox1.Location = new System.Drawing.Point(42, 27);
+            this.groupBox1.Location = new System.Drawing.Point(63, 42);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(988, 526);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(1482, 809);
             this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Kiosk";
             this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(84, 709);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(219, 35);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Log everyone out";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_2);
+            // 
             // currentTime
             // 
             this.currentTime.AutoSize = true;
             this.currentTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.currentTime.Location = new System.Drawing.Point(177, 98);
+            this.currentTime.Location = new System.Drawing.Point(266, 151);
+            this.currentTime.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.currentTime.Name = "currentTime";
-            this.currentTime.Size = new System.Drawing.Size(357, 42);
+            this.currentTime.Size = new System.Drawing.Size(526, 64);
             this.currentTime.TabIndex = 4;
             this.currentTime.Text = "There are 0 visitors";
             // 
             // aboutButton
             // 
             this.aboutButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.aboutButton.Location = new System.Drawing.Point(725, 336);
+            this.aboutButton.Location = new System.Drawing.Point(1088, 517);
+            this.aboutButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.aboutButton.Name = "aboutButton";
-            this.aboutButton.Size = new System.Drawing.Size(134, 69);
+            this.aboutButton.Size = new System.Drawing.Size(201, 106);
             this.aboutButton.TabIndex = 6;
             this.aboutButton.Text = "About";
             this.aboutButton.UseVisualStyleBackColor = true;
@@ -114,17 +131,19 @@
             // 
             this.loggedinCount.AutoSize = true;
             this.loggedinCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.loggedinCount.Location = new System.Drawing.Point(223, 181);
+            this.loggedinCount.Location = new System.Drawing.Point(334, 278);
+            this.loggedinCount.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.loggedinCount.Name = "loggedinCount";
-            this.loggedinCount.Size = new System.Drawing.Size(224, 25);
+            this.loggedinCount.Size = new System.Drawing.Size(320, 37);
             this.loggedinCount.TabIndex = 4;
             this.loggedinCount.Text = "There are 0 visitors.";
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(725, 441);
+            this.button3.Location = new System.Drawing.Point(1088, 678);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(134, 43);
+            this.button3.Size = new System.Drawing.Size(201, 66);
             this.button3.TabIndex = 5;
             this.button3.Text = "Log";
             this.button3.UseVisualStyleBackColor = true;
@@ -133,9 +152,10 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Rosa_Parks_Login_Kiosk2.Properties.Resources.tigercub;
-            this.pictureBox1.Location = new System.Drawing.Point(56, 110);
+            this.pictureBox1.Location = new System.Drawing.Point(84, 169);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(94, 79);
+            this.pictureBox1.Size = new System.Drawing.Size(141, 122);
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
@@ -143,9 +163,10 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(725, 223);
+            this.button2.Location = new System.Drawing.Point(1088, 343);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(134, 84);
+            this.button2.Size = new System.Drawing.Size(201, 129);
             this.button2.TabIndex = 3;
             this.button2.Text = "Logout";
             this.button2.UseVisualStyleBackColor = true;
@@ -156,24 +177,15 @@
             this.timer1.Enabled = true;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(6, 451);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(146, 23);
-            this.button4.TabIndex = 7;
-            this.button4.Text = "Log everyone out";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click_2);
-            // 
             // Form1
             // 
             this.AcceptButton = this.button1;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1065, 584);
+            this.ClientSize = new System.Drawing.Size(1598, 898);
             this.Controls.Add(this.groupBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Rosa Parks Login Kiosk";
             this.Load += new System.EventHandler(this.Form1_Load);
--- Rosa Parks Login Kiosk2/Form2.Designer.cs
+++ Rosa Parks Login Kiosk2/Form2.Designer.cs
@@ -35,24 +35,27 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.roomName = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // parentsName
             // 
             this.parentsName.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.parentsName.Location = new System.Drawing.Point(155, 80);
+            this.parentsName.Location = new System.Drawing.Point(232, 123);
+            this.parentsName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.parentsName.Name = "parentsName";
-            this.parentsName.Size = new System.Drawing.Size(292, 49);
+            this.parentsName.Size = new System.Drawing.Size(436, 70);
             this.parentsName.TabIndex = 0;
             // 
             // checkInText
             // 
             this.checkInText.AutoSize = true;
             this.checkInText.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.checkInText.Location = new System.Drawing.Point(150, 36);
+            this.checkInText.Location = new System.Drawing.Point(225, 55);
+            this.checkInText.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.checkInText.Name = "checkInText";
-            this.checkInText.Size = new System.Drawing.Size(330, 25);
+            this.checkInText.Size = new System.Drawing.Size(476, 37);
             this.checkInText.TabIndex = 1;
             this.checkInText.Text = "Enter your first and last name:";
             this.checkInText.Click += new System.EventHandler(this.checkInText_Click);
@@ -60,9 +63,10 @@
             // checkInButton
             // 
             this.checkInButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.checkInButton.Location = new System.Drawing.Point(359, 241);
+            this.checkInButton.Location = new System.Drawing.Point(538, 371);
+            this.checkInButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.checkInButton.Name = "checkInButton";
-            this.checkInButton.Size = new System.Drawing.Size(121, 54);
+            this.checkInButton.Size = new System.Drawing.Size(182, 83);
             this.checkInButton.TabIndex = 5;
             this.checkInButton.Text = "Check In";
             this.checkInButton.UseVisualStyleBackColor = true;
@@ -71,36 +75,52 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Rosa_Parks_Login_Kiosk2.Properties.Resources.tigercub;
-            this.pictureBox1.Location = new System.Drawing.Point(22, 56);
+            this.pictureBox1.Location = new System.Drawing.Point(33, 86);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(122, 98);
+            this.pictureBox1.Size = new System.Drawing.Size(183, 151);
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
             // 
             // roomName
             // 
             this.roomName.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.roomName.Location = new System.Drawing.Point(155, 174);
+            this.roomName.Location = new System.Drawing.Point(232, 268);
+            this.roomName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.roomName.Name = "roomName";
-            this.roomName.Size = new System.Drawing.Size(292, 49);
+            this.roomName.Size = new System.Drawing.Size(436, 70);
             this.roomName.TabIndex = 4;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(150, 132);
+            this.label1.Location = new System.Drawing.Point(225, 203);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(291, 25);
+            this.label1.Size = new System.Drawing.Size(422, 37);
             this.label1.TabIndex = 5;
             this.label1.Text = "Enter destination or room#";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(85, 387);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(104, 24);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Print label";
+            this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // Form2
             // 
             this.AcceptButton = this.checkInButton;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(586, 319);
+            this.ClientSize = new System.Drawing.Size(879, 491);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.roomName);
             this.Controls.Add(this.pictureBox1);
@@ -108,6 +128,7 @@
             this.Controls.Add(this.checkInText);
             this.Controls.Add(this.parentsName);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form2";
             this.Text = "Check In";
             this.Load += new System.EventHandler(this.Form2_Load);
@@ -125,5 +146,6 @@
 		private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.TextBox roomName;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
